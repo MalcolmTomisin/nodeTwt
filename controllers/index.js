3247cd8eedd7a2cb2a8e89fb64c884a9d6942476
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-26 02:11:56 +0100

    implement mail service

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,25 @@
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/database");
-const jwt = require('jsonwebtoken');
-const config = require('../config');
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+const { validateEmail } = require("../util");
 
 module.exports = {
 	newUser: (req, res, next) => {
 		const formData = req.body;
 		let email = formData.email;
 		let password = formData.password;
-		if (!email) return res.status(200).json({ success: false, status: "You have to provide your email address" });
-		if (!password) return res.status(200).json({ success: false, status: "You have to provide a password" });
+		if (!validateEmail(email)) return res.status(200).json({
+			success: false,
+			status: "You have to provide valid email address"
+		});
+		if (!password) return res.status(200).json({
+			success: false,
+			status: "You have to provide a password"
+		});
+		const salt = bcrypt.genSaltSync(10);
+		if (!salt) {
+			next(new Error("Could not generate a salt"));
+		}
 	}
-}
+};
--- util/index.js
+++ util/index.js
@@ -1,8 +1,54 @@
+
+const mailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = mailer.createTransport({
+	service: "gmail",
+	auth: {
+		// eslint-disable-next-line no-undef
+		user: process.env.EMAIL_USER,
+		// eslint-disable-next-line no-undef
+		pass: process.env.EMAIL_PASS,
+	},
+});
+
 module.exports = {
-	validateEmail: mail => {
-		if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+	validateEmail: (mail) => {
+		if (
+			/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+				mail
+			)
+		) {
 			return true;
 		}
 		return false;
-	}
+	},
+	sendMail: (to, subject, text, callback) => {
+		transporter.sendMail(
+			{
+				from: "malcolmtomisin@gmail.com",
+				to: to,
+				subject: subject,
+				text: text,
+				html: `<html><h3>${text}</h3></html>`,
+			},
+			(err, info) => {
+				callback(err, null);
+				console.log(info.envelope);
+				console.log(info.messageId);
+			}
+		);
+	},
+	idToken: (length) => {
+		//edit the token allowed characters
+		var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split(
+			""
+		);
+		var b = [];
+		for (var i = 0; i < length; i++) {
+			var j = (Math.random() * (a.length - 1)).toFixed(0);
+			b[i] = a[j];
+		}
+		return b.join("");
+	},
 };
