4730737d0ae5b28e423565af242e50132c16fc3b
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-26 01:31:55 +0100

    utils function for validation'

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,10 @@ const config = require('../config');
 
 module.exports = {
 	newUser: (req, res, next) => {
-		
+		const formData = req.body;
+		let email = formData.email;
+		let password = formData.password;
+		if (!email) return res.status(200).json({ success: false, status: "You have to provide your email address" });
+		if (!password) return res.status(200).json({ success: false, status: "You have to provide a password" });
 	}
 }
