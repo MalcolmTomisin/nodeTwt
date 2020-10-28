8363627c4ba5707e313ab49d5e04d792647bf9d7
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-28 16:23:43 +0100

    refactor

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 /* GET home page. */
 
 router.get("/", function(req, res, next) {
-	res.send({index: "Welcome to nothing"});
+	res.send({ index: "Welcome to nothing" });
+	return next;
 });
 
 
